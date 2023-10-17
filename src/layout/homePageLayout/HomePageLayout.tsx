65bb31972f63733188300a8180bc0d154c8acdc5
ebenezeraladesuyi <ebenezeraladesuyi@gmail.com>
2023-10-17 10:24:14 +0100

    head

--- src/layout/homePageLayout/HomePageLayout.tsx
+++ src/layout/homePageLayout/HomePageLayout.tsx
@@ -1,7 +1,8 @@
 // import React from 'react'
 
 import { Outlet } from "react-router-dom"
-import { Footer, Header } from "../../component"
+import { Footer } from "../../component"
+import Header from "../../component/block/homePageBlock/Header"
 
 const HomePageLayout = () => {
   return (
