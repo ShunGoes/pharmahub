1e4b4a91714ed5e1f00989e31104a983e5d5b6e1
antonynans <antonynans@gmail.com>
2023-12-28 15:27:05 +0100

    removed cart page

--- src/routes/AllRoutes.tsx
+++ src/routes/AllRoutes.tsx
@@ -5,87 +5,100 @@ import { lazy } from "react";
 import SelectSigninLayout from "../layout/authLayout/selectAuth/SelectSigninLayout";
 import SelectSignupLayout from "../layout/authLayout/selectAuth/SelectSignupLayout";
 import { UserSigninLayout, UserSignupLayout } from "../layout/authLayout";
-import ShopPage from "../pages/shop/ShopPage";
 import ShopLayout from "../pages/shop/ShopLayout";
-import ShopById from "../pages/shop/shopById";
-
 
 const LandingPage = lazy(() => import("../pages/homePage/LandingPage"));
-const SelectSignin = lazy(() => import("../pages/auth/selectAuthPages/SelectSignin"));
-const SelectSignup = lazy(() => import("../pages/auth/selectAuthPages/SelectSignup"));
-const UserSignin = lazy(() => import("../pages/auth/userAuthPages/UserSignin"))
-const UserSignup = lazy(() => import("../pages/auth/userAuthPages/UserSignup"))
-
+const SelectSignin = lazy(
+  () => import("../pages/auth/selectAuthPages/SelectSignin")
+);
+const SelectSignup = lazy(
+  () => import("../pages/auth/selectAuthPages/SelectSignup")
+);
+const UserSignin = lazy(() => import("../pages/auth/userAuthPages/UserSignin"));
+const UserSignup = lazy(() => import("../pages/auth/userAuthPages/UserSignup"));
+const ShopPage = lazy(() => import("../pages/shop/ShopPage"));
+const ShopById = lazy(() => import("../pages/shop/shopById"));
+const Cart = lazy(() => import("../pages/cart/Cart"));
 
 export const element = createBrowserRouter([
-    {
-        path: "/",
-        element: <HomePageLayout />,
-        children: [
-            {
-                index: true,
-                element: <LandingPage />
-            }
-        ]
-    },
-    {
-        path: "/userSignin",
-        element: <UserSigninLayout />,
-        children: [
-            {
-                index: true,
-                element: <UserSignin />
-            }
-        ]
-    },
-    {
-        path: "/userSignup",
-        element: <UserSignupLayout />,
-        children: [
-            {
-                index: true,
-                element: <UserSignup />
-            }
-        ]
-    },
-    {
-        path: "/selectSignin",
-        element: <SelectSigninLayout />,
-        children: [
-            {
-                index: true,
-                element: <SelectSignin />
-            }
-        ]
-    },
-    {
-        path: "/selectSignup",
-        element: <SelectSignupLayout />,
-        children: [
-            {
-                index: true,
-                element: <SelectSignup />
-            }
-        ]
-    },
-    {
-        path: "/shop",
-        element: <ShopLayout />,
-        children: [
-            {
-                index: true,
-                element: <ShopPage />
-            }
-        ]
-    },
-    {
-        path: "/shopbyid",
-        element: <ShopLayout />,
-        children: [
-            {
-                index: true,
-                element: <ShopById />
-            }
-        ]
-    },
-])
+  {
+    path: "/",
+    element: <HomePageLayout />,
+    children: [
+      {
+        index: true,
+        element: <LandingPage />,
+      },
+    ],
+  },
+  {
+    path: "/userSignin",
+    element: <UserSigninLayout />,
+    children: [
+      {
+        index: true,
+        element: <UserSignin />,
+      },
+    ],
+  },
+  {
+    path: "/userSignup",
+    element: <UserSignupLayout />,
+    children: [
+      {
+        index: true,
+        element: <UserSignup />,
+      },
+    ],
+  },
+  {
+    path: "/selectSignin",
+    element: <SelectSigninLayout />,
+    children: [
+      {
+        index: true,
+        element: <SelectSignin />,
+      },
+    ],
+  },
+  {
+    path: "/selectSignup",
+    element: <SelectSignupLayout />,
+    children: [
+      {
+        index: true,
+        element: <SelectSignup />,
+      },
+    ],
+  },
+  {
+    path: "/shop",
+    element: <ShopLayout />,
+    children: [
+      {
+        index: true,
+        element: <ShopPage />,
+      },
+    ],
+  },
+  {
+    path: "/shopbyid",
+    element: <ShopLayout />,
+    children: [
+      {
+        index: true,
+        element: <ShopById />,
+      },
+    ],
+  },
+  {
+    path: "/cart",
+    element: <ShopLayout />,
+    children: [
+      {
+        index: true,
+        element: <Cart />,
+      },
+    ],
+  },
+]);
